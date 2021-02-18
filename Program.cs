a2583f9fe783ec1aafa7f58421cb3f6398e2f488
Daniel Antunes <danielxts555@gmail.com>
2021-02-17 22:29:53 -0300

    Git init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ValoresRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo lados = new Retangulo();
+
+            Console.WriteLine("Entre a Largura e altura do retângulo:");
+            lados.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            lados.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Area:" + lados.Area());
+            Console.WriteLine("Perimetro:" + lados.Perimetro());
+            Console.WriteLine("Perimetro:" + lados.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+namespace ValoresRetangulo
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+
+    public double Area()
+        {
+            return Largura * Altura;
+        } 
+
+    public double Perimetro()
+        {
+            return (Largura + Altura) * 2.0;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Altura * Altura + Largura * Largura);
+        }
+
+        
+    }
+    
+}
